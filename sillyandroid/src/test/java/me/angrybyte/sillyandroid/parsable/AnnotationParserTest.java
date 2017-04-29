a6f0909d8a51feeb0ee807f327e65636bedca74e
Milos Marinkovic <milosmns@gmail.com>
2017-04-29 18:04:46 +0200

    - Adding Robolectric tests - Cleanup
    (cherry picked from commit 238b510)

--- sillyandroid/src/test/java/me/angrybyte/sillyandroid/parsable/AnnotationParserTest.java
+++ sillyandroid/src/test/java/me/angrybyte/sillyandroid/parsable/AnnotationParserTest.java
@@ -1,18 +1,27 @@
 package me.angrybyte.sillyandroid.parsable;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.appcompat.BuildConfig;
+import android.view.View;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 
-import me.angrybyte.sillyandroid.BuildConfig;
-
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
@@ -62,7 +71,87 @@ public class AnnotationParserTest {
             return mObjectField;
         }
     }
+
+    /**
+     * Mock implementation of the {@link LayoutWrapper}.
+     */
+    private static final class MockLayoutWrapper implements LayoutWrapper {
+
+        private WeakReference<Context> mContextRef;
+
+        /**
+         * The default constructor.
+         *
+         * @param context A context to use for View inflation
+         */
+        MockLayoutWrapper(@NonNull final Context context) {
+            mContextRef = new WeakReference<>(context);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        @SuppressWarnings({ "ResourceType", "unchecked" })
+        public <ViewType extends View> ViewType findView(@IdRes final int viewId) {
+            if (viewId == 1) {
+                return (ViewType) new View(mContextRef.get());
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A test activity with annotation-injectable menu and layout fields.
+     */
+    @Annotations.Menu(1)
+    @Annotations.Layout(2)
+    @SuppressWarnings("ResourceType")
+    private static final class TestMenuLayoutActivity extends AppCompatActivity {
+
+        @SuppressWarnings("unused")
+        private int mMenuId;
+
+        @SuppressWarnings("unused")
+        private int mLayoutId;
+
+        /**
+         * Gets the menu ID value passed through the annotation.
+         *
+         * @return An integer ID
+         */
+        public int getMenuId() {
+            return mMenuId;
+        }
+
+        /**
+         * Gets the layout ID value passed through the annotation.
+         *
+         * @return An integer ID
+         */
+        public int getLayoutId() {
+            return mLayoutId;
+        }
+    }
     // </editor-fold>
+
+    private Activity mActivity;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Before
+    public void setUp() {
+        mActivity = Robolectric.setupActivity(TestMenuLayoutActivity.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @After
+    public void tearDown() {
+        mActivity = null;
+    }
 
     /**
      * Tests the {@link AnnotationParser#getAllFields(Class)} method using {@link TestParsableClass}.
@@ -101,6 +190,17 @@ public class AnnotationParserTest {
             }
         }
         return false;
+    }
+
+    /**
+     * Tests the {@link AnnotationParser#parseType(Context, Object)} method.
+     */
+    @Test
+    public void testParseType() {
+        final TestMenuLayoutActivity activity = (TestMenuLayoutActivity) mActivity;
+        AnnotationParser.parseType(mActivity, activity);
+        assertEquals("Menu ID not injected properly", activity.getMenuId(), 1);
+        assertEquals("Layout ID not injected properly", activity.getLayoutId(), 2);
     }
 
 }
