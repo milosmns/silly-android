068f5f26fc7c5e76b11ebef9c3a12a325ef852ee
Milos Marinkovic <milosmns@gmail.com>
2017-04-29 15:14:01 +0200

    Splitting view parsing test into two methods

--- sillyandroid/src/test/java/me/angrybyte/sillyandroid/parsable/AnnotationParserTest.java
+++ sillyandroid/src/test/java/me/angrybyte/sillyandroid/parsable/AnnotationParserTest.java
@@ -1,17 +1,20 @@
 package me.angrybyte.sillyandroid.parsable;
 
+import android.support.annotation.NonNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
 
 import me.angrybyte.sillyandroid.BuildConfig;
 
-import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * A set of tests related to the {@link me.angrybyte.sillyandroid.parsable.AnnotationParser}.
@@ -62,20 +65,42 @@ public class AnnotationParserTest {
     // </editor-fold>
 
     /**
-     * Tests the {@link AnnotationParser#getAllFields(Class)} method.
+     * Tests the {@link AnnotationParser#getAllFields(Class)} method using {@link TestParsableClass}.
      */
     @Test
-    public void testGetAllFields() {
-        List<Field> fields = AnnotationParser.getAllFields(TestParsableClass.class);
-        assertNotNull("[1] Fields list is null", fields);
-        assertEquals("[1] Fields list is not parsed correctly", fields.size(), 1);
-        assertEquals("[1] Field 'mIntField' not found", fields.get(0).getName(), "mIntField");
+    public void testGetAllFieldsBase() {
+        final List<Field> baseFields = AnnotationParser.getAllFields(TestParsableClass.class);
+        final String baseString = Arrays.toString(baseFields.toArray());
+        assertNotNull("[1] Fields list is null", baseFields);
+        assertTrue("[1] Fields list is not parsed correctly: " + baseString, baseFields.size() >= 1);
+        assertTrue("[1] Field 'mIntField' not found: " + baseString, containsField(baseFields, "mIntField"));
+    }
 
-        fields = AnnotationParser.getAllFields(TestFinalParsableClass.class);
-        assertNotNull("[2] Fields list is null", fields);
-        assertEquals("[2] Fields list is not parsed correctly", fields.size(), 2);
-        assertEquals("[2] Field 'mObjectField' not found", fields.get(0).getName(), "mObjectField");
-        assertEquals("[2] Field 'mIntField' not found", fields.get(1).getName(), "mIntField");
+    /**
+     * Tests the {@link AnnotationParser#getAllFields(Class)} method using {@link TestFinalParsableClass}.
+     */
+    @Test
+    public void testGetAllFieldsExtended() {
+        final List<Field> extFields = AnnotationParser.getAllFields(TestFinalParsableClass.class);
+        final String extString = Arrays.toString(extFields.toArray());
+        assertNotNull("[2] Fields list is null", extFields);
+        assertTrue("[2] Fields list is not parsed correctly: " + extString, extFields.size() >= 2);
+        assertTrue("[2] Field 'mIntField' not found: " + extString, containsField(extFields, "mIntField"));
+        assertTrue("[2] Field 'mObjectField' not found: " + extString, containsField(extFields, "mObjectField"));
+    }
+
+    /**
+     * Checks if given fields list contain the given field name.
+     *
+     * @return {@code True} if the given list contains the field, {@code false} if not
+     */
+    private boolean containsField(@NonNull final List<Field> fieldList, @NonNull final String fieldName) {
+        for (int i = 0; i < fieldList.size(); i++) {
+            if (fieldList.get(i).getName().equals(fieldName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
