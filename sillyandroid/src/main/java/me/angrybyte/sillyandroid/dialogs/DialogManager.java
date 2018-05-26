3450a9afff71572c479c4844ca23b3e5d6ffbaac
Milos Marinkovic <milosmns@gmail.com>
2018-05-26 18:08:09 +0200

    Initial work on #1 (managed dialogs): Adding some planks
    (cherry picked from commit d5d1751)

---
+++ sillyandroid/src/main/java/me/angrybyte/sillyandroid/dialogs/DialogManager.java
@@ -0,0 +1,57 @@
+package me.angrybyte.sillyandroid.dialogs;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+
+public interface DialogManager {
+
+    interface DialogManagerCallback {
+
+        @Nullable
+        Dialog onCreateDialog(final int dialogId, @Nullable final Bundle config);
+
+        @Nullable
+        DialogFragment onCreateDialogFragment(final int dialogId, @Nullable final Bundle config);
+    }
+
+    interface DialogManagerListener {
+
+        void onDialogShown(final int dialogId);
+
+        void onDialogDismissed(final int dialogId);
+    }
+
+    void setCallback(@Nullable final DialogManagerCallback callback);
+
+    void setListener(@Nullable final DialogManagerListener listener);
+
+    void showDialog(final int dialogId);
+
+    void showDialog(final int dialogId, @Nullable final Bundle config);
+
+    void showDialogFragment(final int dialogId);
+
+    void showDialogFragment(final int dialogId, @Nullable final Bundle config);
+
+    boolean isDialogShowing(final int dialogId);
+
+    void dismissDialog(final int dialogId);
+
+    @NonNull
+    Parcelable saveState();
+
+    void restoreState(@Nullable final Parcelable state, final boolean showNow);
+
+    void recreateAll();
+
+    void showAll();
+
+    void hideAll();
+
+    void dismissAll();
+
+}
