4182467d9a8079f073c0a6b79ce19ba818508ecb
Milos Marinkovic <milosmns@gmail.com>
2018-05-29 06:19:42 +0200

    Working on #1: Adding a stub test

---
+++ sillyandroid/src/test/java/me/angrybyte/sillyandroid/dialogs/DialogManagerImplTest.java
@@ -0,0 +1,67 @@
+package me.angrybyte.sillyandroid.dialogs;
+
+import org.junit.Test;
+
+public class DialogManagerImplTest {
+
+    @Test
+    public void setCallback() {
+    }
+
+    @Test
+    public void setListener() {
+    }
+
+    @Test
+    public void showDialog() {
+    }
+
+    @Test
+    public void showDialog1() {
+    }
+
+    @Test
+    public void showDialogFragment() {
+    }
+
+    @Test
+    public void showDialogFragment1() {
+    }
+
+    @Test
+    public void isDialogShowing() {
+    }
+
+    @Test
+    public void dismissDialog() {
+    }
+
+    @Test
+    public void saveState() {
+    }
+
+    @Test
+    public void restoreState() {
+    }
+
+    @Test
+    public void recreateAll() {
+    }
+
+    @Test
+    public void unhideAll() {
+    }
+
+    @Test
+    public void hideAll() {
+    }
+
+    @Test
+    public void dismissAll() {
+    }
+
+    @Test
+    public void dispose() {
+    }
+
+}
